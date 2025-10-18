a981d9c6b2f520b4a27bd2d28378d8041c7ea2bf
Mohammad Al-Rifaii <mohammad.alrifaii@tuum.com>
2025-10-18 23:48:49 +0300

    some name improvements

--- src/main/java/org/rifaii/dbrng/Generator.java
+++ src/main/java/org/rifaii/dbrng/Generator.java
@@ -14,20 +14,20 @@ import java.util.stream.IntStream;
 
 public class Generator {
 
-    private static final Random random = new Random();
-
-    static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS'Z'");
-
-    static char[] alphanumerics = {
+    private static final Random RANDOM = new Random();
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS'Z'");
+    private static final char[] ALPHANUMERICS = {
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
         '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
     };
 
+    private Generator() {}
+
     static char[] generateString(int length) {
         char[] chars = new char[length];
         for (int i = 0;i < length;i++) {
-            chars[i] = alphanumerics[random.nextInt(alphanumerics.length)];
+            chars[i] = ALPHANUMERICS[RANDOM.nextInt(ALPHANUMERICS.length)];
         }
         return chars;
     }
@@ -35,7 +35,7 @@ public class Generator {
     public static CsvRowIterator generate(List<Column> columnDetails, int rowsNum) {
         List<Supplier<String>> plan = new ArrayList<>();
         var random = new Random(System.currentTimeMillis());
-        String formattedDate = formatter.format(LocalDateTime.now());
+        String formattedDate = DATE_FORMATTER.format(LocalDateTime.now());
 
         columnDetails.forEach(c -> {
             Queue<Integer> ids = new ArrayDeque<>();
@@ -58,5 +58,4 @@ public class Generator {
 
         return new CsvRowIterator(rowsNum, plan);
     }
-
 }
